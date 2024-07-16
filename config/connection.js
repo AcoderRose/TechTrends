98a448fe5fa5e719c1d9437e9a1e247138d0bf69
AcoderRose <addyro96@att.net>
2024-07-15 22:39:26 -0700

    Made changes to connection.js file. Debugging deploying to render still not working.

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,14 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-      }
-    );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST, // Ensure this is the correct host
+    dialect: "postgres",
+  }
+);
 
 module.exports = sequelize;
