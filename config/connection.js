a1eb975c1cc295dd4dbbb9c683357d01750b6129
AcoderRose <addyro96@att.net>
2024-07-16 13:19:13 -0700

    Made changes to connection.js and .env files. Trying to debug and refactor to get project deployed on render.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST, // Ensure this is the correct host
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+});
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST, // Ensure this is the correct host
+//     dialect: "postgres",
+//   }
+// );
 
 module.exports = sequelize;
